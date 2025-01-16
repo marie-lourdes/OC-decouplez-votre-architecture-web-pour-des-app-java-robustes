7317b2f689754990bdcfd0ceece2ce0b20dde379
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-01-16 15:37:22 +0100

    fix version spring boo and java

--- src/main/java/com/airbusiness/airbusiness_mvc/AirbusinessMvcApplication.java
+++ src/main/java/com/airbusiness/airbusiness_mvc/AirbusinessMvcApplication.java
@@ -1,4 +1,4 @@
-package com.airbusiness.airbusinessmvc;
+package com.airbusiness.airbusiness_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/airbusiness/airbusiness_mvc/AirbusinessMvcApplicationTests.java
+++ src/test/java/com/airbusiness/airbusiness_mvc/AirbusinessMvcApplicationTests.java
@@ -1,4 +1,4 @@
-package com.airbusiness.airbusinessmvc;
+package com.airbusiness.airbusiness_mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
