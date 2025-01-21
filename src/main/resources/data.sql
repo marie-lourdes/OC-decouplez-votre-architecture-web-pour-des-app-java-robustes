9c5bc854eb1cfb43d51153cc9391388e55f54b31
marie-lourdes <marielourdesleperlier64@gmail.com>
2025-01-21 10:45:29 +0100

    fix data.sql and insertion sql with configuration spring.jpa.hibernate in application.properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE RESERVATION( 
+id int NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+name VARCHAR(100) ,
+destination VARCHAR(150),
+departure_date VARCHAR(100),
+return_date VARCHAR(15) 
+);
+
+INSERT INTO RESERVATION VALUES (1,'hedh','hhjgj','hjjj','hbhj');
